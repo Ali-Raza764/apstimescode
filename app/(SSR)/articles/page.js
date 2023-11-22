c85f3fcd9f5c88b2befa033dcd1f28ddf25f1e85
Ali-Raza764 <alikillerno@gmail.com>
2023-11-21 17:50:40 -0800

    changes

--- app/(SSR)/articles/page.js
+++ app/(SSR)/articles/page.js
@@ -1,26 +1,30 @@
 import { BlogItem } from "@/components";
 import React from "react";
-import {
-  getFirestore,
-  collection,
-  query,
-  orderBy,
-  getDocs,
-} from "firebase/firestore";
-import { app } from "@/config/firebase";
+// import {
+//   getFirestore,
+//   collection,
+//   query,
+//   orderBy,
+//   getDocs,
+// } from "firebase/firestore";
+// import { app } from "@/config/firebase";
 
-const getArticleData = async () => {
-  const db = getFirestore(app);
-  const q = query(collection(db, "blogs"), orderBy("title", "asc"));
-  const querySnapshot = await getDocs(q);
-  const docs = [];
-  querySnapshot.forEach((doc) => {
-    docs.push(doc.data());
-  });
-  return docs;
-};
+// const getArticleData = async () => {
+//   const db = getFirestore(app);
+//   const q = query(collection(db, "blogs"), orderBy("title", "asc"));
+//   const querySnapshot = await getDocs(q);
+//   const docs = [];
+//   querySnapshot.forEach((doc) => {
+//     docs.push(doc.data());
+//   });
+//   return docs;
+// };
 const page = async() => {
-  const ArticleData = await getArticleData();
+  const apiUrl = process.env.NODE_ENV === 'development' ? "http://localhost:3000/api/getAllBlogs" : "https://apstimesblog.netlify.app";
+  const sample = await fetch(apiUrl, { next:{revalidate:100} });
+  const alldata = await sample.json();
+  const ArticleData = alldata.message || []; // check that the data exists and it is an array
+  // const ArticleData = await getArticleData();
 
   return (
     <section>
